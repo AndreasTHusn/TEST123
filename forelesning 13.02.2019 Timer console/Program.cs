d352a2bf2220786b8795d1d121f45eccc641a4ac
AndreasTH97 <46328048+AndreasTH97@users.noreply.github.com>
2019-02-25 15:01:33 +0100

    Importing progject

---
+++ forelesning 13.02.2019 Timer console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace forelesning_13._02._2019
+{
+    class Program
+    {
+        static void SkrivTid(object kilde, ElapsedEventArgs argument)
+        {
+            Console.WriteLine(argument.SignalTime);
+        }
+
+        static void SkrivTest(object kilde, ElapsedEventArgs argument)
+        {
+            Console.WriteLine("Test");
+        }
+
+        static void Main(string[] args)
+        {
+            Timer minTimer = new Timer(1000);
+            minTimer.Elapsed += new ElapsedEventHandler(SkrivTid);
+            minTimer.Elapsed += new ElapsedEventHandler(SkrivTest);
+            minTimer.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
